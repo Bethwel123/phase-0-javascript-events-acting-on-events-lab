bae293e6c2cb63f8725796df97b8cd99c8759d18
Bethwel123 <bethwellangat654@gmail.com>
2024-06-22 17:58:10 +0300

    Moving things in index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+function moveDodgerRight() {
+    let dodger = document.getElementById("dodger");
+    let rightNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(rightNumbers, 10);
+    console.log(dodger.style.left);
+    dodger.style.left = `${left + 1}px`;
+}
